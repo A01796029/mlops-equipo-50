1c55d2313e307ca133c198a097eb2f8363742e7e
AaronCG01 <73603902+AaronCG01@users.noreply.github.com>
2025-11-14 00:31:55 -0600

    src/__main__.py modified to include the correct 'make_dataset' script

--- src/__main__.py
+++ src/__main__.py
@@ -6,14 +6,15 @@
 import typer
 
 from src import config
-from src import dataset
+# from src import dataset
+from src.data import make_dataset as data_mod
 from src import features
 from src import plots
 from src.modeling import train as train_mod
 from src.modeling import predict as predict_mod
 
 app = typer.Typer()
-app.add_typer(dataset.app, name="data")
+app.add_typer(data_mod.app, name="data") # Antes: dataset.app
 app.add_typer(features.app, name="features")
 app.add_typer(plots.app, name="plots")
 app.add_typer(train_mod.app, name="train")
